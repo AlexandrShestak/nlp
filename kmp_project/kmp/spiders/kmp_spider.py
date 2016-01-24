d5c1778e3c1982fd0e6d0060240c22b3451ae5cd
AlexandrShestak <shestakalexandr@mail.ru>
2016-01-24 22:25:22 +0300

    fix code. now it take all information that i need, but only from one page

--- kmp_project/kmp/spiders/kmp_spider.py
+++ kmp_project/kmp/spiders/kmp_spider.py
@@ -19,15 +19,9 @@ class KillMePleaseSpider(Spider):
         count = 0
         for post in posts:
             item = KmpItem()
-            """item['post_text'] = post.xpath(
-                '/html/body/div[@class="container"]/div/div[@id="stories"]'
-                '/div[@class="row"]/div[@class="col-xs-12" '
-                'and contains(@style,"margin:0.5em 0;line-height:1.785em")]/text()').extract()[0]
-            item['stars'] = post.xpath(
-                '/html/body/div[@class="container"]/div/div[@id="stories"]/'
-                'div[@class="row"]/div[@class="col-xs-12"]/div/b').extract()[0]"""
-
-            item['post_text'] = post.xpath('text()').extract()[0]
-            item['stars'] = stars[count].extract()
+            item['post_text'] = " ".join(post.xpath('text()').extract())
+            item['stars'] = stars[count].xpath('text()').extract()
+            '''item['post_text'] = [s.encode("utf-8") for s in post.xpath('text()').extract()[0]]
+            item['stars'] = [s.encode("utf-8") for s in stars[count].extract()]'''
             count += 1
             yield item

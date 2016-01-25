d85388dbb5d4575aff60ccbc22e8fd4f0ad86928
AlexandrShestak <shestakalexandr@mail.ru>
2016-01-25 23:49:25 +0300

    now it parse some constant number of pages. add delay before parse page

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 To run scraper :
 scrapy crawl kmp -o items.json -t json
+or
+scrapy crawl kmp -o items.csv -t csv
--- kmp_project/kmp/spiders/kmp_spider.py
+++ kmp_project/kmp/spiders/kmp_spider.py
@@ -1,16 +1,26 @@
 from scrapy import Spider
 from scrapy.selector import Selector
+from scrapy.http import Request
+import time
 from kmp.items import KmpItem
 
+
+START_PAGE_NUMBER = 1710
+PAGE_COUNT = 10
 
 class KillMePleaseSpider(Spider):
     name = "kmp"
     allowed_domains = ["http://killpls.me/"]
-    start_urls = ["http://killpls.me"]
+    #start_urls = ["http://killpls.me/page/" + START_PAGE_NUMBER]
+
+
+    def start_requests(self):
+        for page_number in range(START_PAGE_NUMBER - PAGE_COUNT, START_PAGE_NUMBER):
+            yield Request("http://killpls.me/page/" + str(page_number),  callback=self.parse)
 
     def parse(self, response):
         posts = Selector(response).xpath(
-            '/html/body/div[@class="container"]/div/div[@id="stories"]'
+            '/html/body/div[    @class="container"]/div/div[@id="stories"]'
             '/div[@class="row"]/div[@class="col-xs-12" '
             'and contains(@style,"margin:0.5em 0;line-height:1.785em")]')
         stars = Selector(response).xpath(
@@ -25,3 +35,4 @@ class KillMePleaseSpider(Spider):
             item['stars'] = [s.encode("utf-8") for s in stars[count].extract()]'''
             count += 1
             yield item
+        time.sleep(5)

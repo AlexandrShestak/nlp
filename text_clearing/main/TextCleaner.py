53fe89a202cdaddd9392c790f74f5912bff214f6
AlexandrShestak <shestakalexandr@mail.ru>
2016-01-29 00:19:06 +0300

    delete russian stop words, but i think it is need to expand russian stop words dictionary

--- text_clearing/main/TextCleaner.py
+++ text_clearing/main/TextCleaner.py
@@ -6,12 +6,12 @@ reload(sys)
 sys.setdefaultencoding("utf-8")
 from nltk.corpus import stopwords
 import nltk
-
-nltk.download()
+from stop_words import get_stop_words
 
 with open('items.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
+        text_before_cleaning = row['post_text']
         post_text = row['post_text']
         print(post_text)
         exclude = set(string.punctuation)
@@ -23,8 +23,33 @@ with open('items.csv') as csvfile:
         # for word in post_words:
         #     rs = RussianStemmer()
         #     print(word + " " + rs.stem( unicode(word, "utf-8")))
-        filtered_words = [word for word in post_words if word not in stopwords.words('russian')]
-        print("delete stop words")
-        for word in filtered_words:
-            print(word)
+
+        stop_words = get_stop_words('ru')
+        for sw in stop_words:
+            print(sw)
+
+        text_after_cleaning = ""
+        for word in post_words:
+             if word not in stop_words:
+                text_after_cleaning += word + " "
+
+
+        print("text before cleaning")
+        print(text_before_cleaning)
+        print("text after delete stop words")
+        print(text_after_cleaning)
+        post_words = text_after_cleaning.split(" ")
+        text_after_stemming = ""
+        rs = RussianStemmer()
+        for word in post_words:
+            text_after_stemming += rs.stem(unicode(word, "utf-8")) + " "
+        print("text after STEMMING")
+        print(text_after_stemming)
+
+
+        # filtered_words = [word for word in post_words if word not in stop_words]
+        # print("delete stop words")
+
+        # for word in filtered_words:
+        #     print(word)
         post_stars = row['stars']

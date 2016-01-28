e8299b8f94e5ad7438ecf9d8115bff86ef258785
AlexandrShestak <shestakalexandr@mail.ru>
2016-01-28 23:56:46 +0300

    try to delete russian stop words

--- text_clearing/main/TextCleaner.py
+++ text_clearing/main/TextCleaner.py
@@ -4,6 +4,10 @@ from nltk.stem.snowball import RussianStemmer
 import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
+from nltk.corpus import stopwords
+import nltk
+
+nltk.download()
 
 with open('items.csv') as csvfile:
     reader = csv.DictReader(csvfile)
@@ -16,7 +20,11 @@ with open('items.csv') as csvfile:
         post_text = ''.join([i for i in post_text if not i.isdigit()])
         print(post_text)
         post_words = post_text.split()
-        for word in post_words:
-            rs = RussianStemmer()
-            print(word + " " + rs.stem( unicode(word, "utf-8")))
+        # for word in post_words:
+        #     rs = RussianStemmer()
+        #     print(word + " " + rs.stem( unicode(word, "utf-8")))
+        filtered_words = [word for word in post_words if word not in stopwords.words('russian')]
+        print("delete stop words")
+        for word in filtered_words:
+            print(word)
         post_stars = row['stars']

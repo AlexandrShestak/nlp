dee7d8c4c577bc5bad09a8131a477767bbb358fd
AlexandrShestak <shestakalexandr@mail.ru>
2016-01-30 17:41:26 +0300

    refactoring

--- text_clearing/main/TextCleaner.py
+++ text_clearing/main/TextCleaner.py
@@ -4,8 +4,6 @@ from nltk.stem.snowball import RussianStemmer
 import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
-from nltk.corpus import stopwords
-import nltk
 from stop_words import get_stop_words
 
 with open('items.csv') as csvfile:
@@ -13,43 +11,16 @@ with open('items.csv') as csvfile:
     for row in reader:
         text_before_cleaning = row['post_text']
         post_text = row['post_text']
-        print(post_text)
         exclude = set(string.punctuation)
         post_text = ''.join(ch for ch in post_text if ch not in exclude)
-        print(post_text)
         post_text = ''.join([i for i in post_text if not i.isdigit()])
-        print(post_text)
         post_words = post_text.split()
-        # for word in post_words:
-        #     rs = RussianStemmer()
-        #     print(word + " " + rs.stem( unicode(word, "utf-8")))
-
         stop_words = get_stop_words('ru')
-        for sw in stop_words:
-            print(sw)
-
-        text_after_cleaning = ""
-        for word in post_words:
-             if word not in stop_words:
-                text_after_cleaning += word + " "
-
-
-        print("text before cleaning")
-        print(text_before_cleaning)
-        print("text after delete stop words")
-        print(text_after_cleaning)
-        post_words = text_after_cleaning.split(" ")
-        text_after_stemming = ""
+        words_after_deleting_stop_words = [w for w in post_text.split() if not w in stop_words]
+        print(words_after_deleting_stop_words)
         rs = RussianStemmer()
-        for word in post_words:
-            text_after_stemming += rs.stem(unicode(word, "utf-8")) + " "
-        print("text after STEMMING")
-        print(text_after_stemming)
-
-
-        # filtered_words = [word for word in post_words if word not in stop_words]
-        # print("delete stop words")
-
-        # for word in filtered_words:
-        #     print(word)
+        words_after_stemming = [rs.stem(unicode(w, "utf-8")) for w in words_after_deleting_stop_words]
+        print(words_after_stemming)
+        text_after_cleaning = ' '.join(words_after_stemming)
+        print(text_after_cleaning)
         post_stars = row['stars']
